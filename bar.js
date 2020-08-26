e1a34743046f176855234b0ae633db7b1a23a884
OkayKenji <70079754+OkayKenji@users.noreply.github.com>
2020-08-26 00:57:29 -0400

    Update
    Update

--- bar.js
+++ bar.js
@@ -15,33 +15,30 @@
  *  x axis
  *  
  *  @param xLoc Where on the x-axis is located
- *  @param n    The height "n" of the line.
+ *  @param n The height "n" of the line
  *
  *  When getting the values xLoc and n they should be the name of the object say "y"
  ^  and followed ".x" or ".length" for example y.x; or y.length; 
  */
 class bar {
   constructor(xLoc, n) {
-    this.x = xLoc;
     this.length = n;
-    this.barColor = this.colorGradient();
+    this.barColor = this.colorGradient(xLoc);
   }
 
   /**Place, places a line and also keeps track of how much the line was moved. 
    */
-  place() {
+  place(x) {
     strokeWeight(0);
     stroke(this.barColor);
     fill(this.barColor);
-    //line(this.x, height, this.x, height - this.length);
-    
-    rect(this.x,height-(this.length/2),barWidth,this.length);
+    rect(x,height-(this.length/2),barWidth,this.length);
   }
 
   /**colorGradient 
    */
-  colorGradient() {
-    let temp = map(this.x, width, 0, 0, 255);
+  colorGradient(xLoc) {
+    let temp = map(xLoc, width, 0, 0, 255);
     if (colorScheme == "Red") {
       return color(255, temp, temp);
     } else if (colorScheme == "Green") {
